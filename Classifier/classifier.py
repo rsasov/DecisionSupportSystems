c57c17d5b616c77f73dc55737f790700c3fdf59f
Eki <eki@localhost.localdomain>
2017-06-04 19:17:05 +0200

    added jupyter visualization

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from sklearn import svm
 import random
 
@@ -14,30 +15,33 @@ with open('data.txt', 'r') as f:
 classes = {e[0] for e in lines}
 words = {w for e in lines for w in e[1].rstrip().split()}
 
-proportion = int(len(words)*0.9)
-train_words = set(random.sample(words, proportion))
-test_words = words - train_words
-
 #print classes
-print train_words
-print "\n\n"
-print test_words
+#print words
 
 ind_Y = {key:ind for ind,key in enumerate(classes)}
-#ind_X = {key:ind for ind,key in enumerate(words)}
-ind_X_train = {key:ind for ind,key in enumerate(train_words)}
-ind_X_test = {key:ind for ind,key in enumerate(test_words)}
+ind_X = {key:ind for ind,key in enumerate(words)}
 #print(ind_Y)
 #print(ind_X)
 
 Y = [ind_Y[l[0]] for l in lines]
 #print(Y) 
 
-X = [vectorize(ind_X_train, l[1]) for l in lines]
-X_test = [vectorize(ind_X_test, l[1]) for l in lines]
+X = [vectorize(ind_X, l[1]) for l in lines]
+
+
+shuffled = zip(X, Y)
+random.shuffle(shuffled)
+training = shuffled[len(shuffled)//10:]
+test = shuffled[:len(shuffled)//10]
+x_test, y_test = zip(*test)
+x_training, y_training = zip(*training)
 #print(X)
 
 clf = svm.SVC()
-clf.fit(X, Y)
+clf.fit(x_training, y_training)
 
-print(clf.predict(X_test))
+#accuracy = sum(clf.predict(x) == y for x,y in zip(x_test, y_test)) / len(y_test)
+#print accuracy
+
+accuracy = sum(clf.predict(x) == y for x,y in zip(x_training, y_training)) / len(y_training)
+print accuracy
